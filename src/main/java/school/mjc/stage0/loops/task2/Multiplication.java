5d47137516e2b6305c6e8291acde2b149e269fdd
alikeli <eli-com2@yandex.ru>
2023-03-08 19:18:50 +0100

    done

--- src/main/java/school/mjc/stage0/loops/task2/Multiplication.java
+++ src/main/java/school/mjc/stage0/loops/task2/Multiplication.java
@@ -2,18 +2,9 @@ package school.mjc.stage0.loops.task2;
 
 public class Multiplication {
     public void printMultiplied(int multiplyByAndToInclusive) {
-        int i =0;
-        while (i <= multiplyByAndToInclusive) {
-            System.out.println(i*multiplyByAndToInclusive);
-            i++;
-       }
-//        while (i >= multiplyByAndToInclusive) {
-//            System.out.println(i*multiplyByAndToInclusive);
-//            i--;
-//        }
-//        while (i ==0) {
-//            System.out.println("");
-//            i++;
-//        }
+        int i = 0;
+        while(i <= Math.abs(multiplyByAndToInclusive) && multiplyByAndToInclusive != 0){
+            System.out.println(i++*multiplyByAndToInclusive);
+        }
     }
 }
--- src/main/java/school/mjc/stage0/loops/task2/PrimeNumbers.java
+++ src/main/java/school/mjc/stage0/loops/task2/PrimeNumbers.java
@@ -2,16 +2,23 @@ package school.mjc.stage0.loops.task2;
 
 public class PrimeNumbers {
     public void printPrimeNumbers(int printToInclusive) {
-
         int i = 2;
-        while (printToInclusive % i == 0) {
-            System.out.println(i);
-
+        while(i<=printToInclusive){
+            int p = 2, count = 0;
+            while (p <= Math.pow(i, 1.0/2)){
+                if(i % p == 0){
+                    count ++;
+                    break;
+                }
+                p++;
+            }
+            if (count == 0){
+                System.out.println(i);
+            }
             i++;
-
+        }
         }
 
-    }
 
     public static void main(String[] args) {
         PrimeNumbers p = new PrimeNumbers();
