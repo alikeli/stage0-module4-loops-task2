3f86a430f5c8865f2199f4640da797956dbb2120
alikeli <eli-com2@yandex.ru>
2023-03-08 19:13:32 +0100

    done

--- src/main/java/school/mjc/stage0/loops/task2/WordsBuilder.java
+++ src/main/java/school/mjc/stage0/loops/task2/WordsBuilder.java
@@ -2,7 +2,9 @@ package school.mjc.stage0.loops.task2;
 
 public class WordsBuilder {
     public void buildPhrase(char... chars) {
-
+        for (char i: chars){
+            System.out.print(i);
+        }
 
     }
 }
